c35e04edcfa880fe693e56d5656e37b7599938b4
Blake Lockley <blakelockley@Blakes-MacBook-Pro.local>
2016-03-26 16:25:56 +1100

    Initial Commit

---
+++ FSA/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FSA
+//
+//  Created by Blake Lockley on 26/03/2016.
+//  Copyright © 2016 Blake Lockley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
